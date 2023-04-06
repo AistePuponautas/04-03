ea3e6da8cc35e7c1a3468fbeda59ed09ef6a0900
AistePuponautas <122285130+AistePuponautas@users.noreply.github.com>
2023-04-06 15:13:31 +0300

    Add files via upload

---
+++ 2023-04-03 hw/app.js
@@ -0,0 +1,176 @@
+//---------Dviratininkas---------------
+
+class Color {
+
+    constructor() {
+        this.color = '#' + Math.floor(Math.random()*16777215).toString(16).padEnd(6, '0');
+    }
+}
+
+//---------Bebras---------------
+
+class NicePrint extends Color {
+
+    constructor() {
+        super()
+    }
+
+    printColor() {
+        console.log('%c ' + this.constructor.forest + ' ', 'background-color:' + this.color + ';')
+    }
+
+}
+
+
+// ---------Antanas--------------
+
+class Animal extends NicePrint {
+
+    static forest = 'B I G';
+
+    constructor(age) {
+        super();
+        if(!age) {
+            throw new Error ('Animal needs age!')
+        } else {
+            this.age = age;
+        }
+        
+    }
+
+    printColor2() {
+        console.log('Hello')
+    }
+
+
+}
+
+//------------Ieva---------
+
+const animal1 = new Animal(20);
+const animal2 = new Animal(17);
+
+// animal1.color = 'pink';
+
+Animal.forest = 'small'
+
+// animal2.printColor();
+// animal1.printColor()
+
+// console.log(animal1, animal2);
+
+// console.log(Animal.forest)
+
+// ------------- ///// -------------------
+
+// 1. Sukurti klasę Kibiras1. Konstruktoriuje sukurti savybę 
+// akmenuKiekis  kuri lygi 0. Parašyti šiai klasei metodus, 
+// pridedančius akmenis: prideti1Akmeni() pridetiDaugAkmenu(kiekis) 
+// ir metodą išvedantį akmenų kiekį į konsolę- kiekPririnktaAkmenu(). 
+// Sukurti vieną kibiro objektą ir pademonstruoti akmenų rinkimą į 
+// kibirą ir rezultatų išvedimą.
+
+
+// 7. (STATIC) Klasėje Kibiras1 (pirmas uždavinys) sukurti 
+// metodą akmenuSkaiciusVisuoseKibiruose(), kuris rodytų bendrą 
+// visuose kibiruose pririnktų akmenų kiekį (visuose sukurtuose 
+// Kibiras objektuose). Skaičiuoti akmenim, naudokite statine savybe
+// visiAkmenys (kurioje yra 
+// įrašytas ir saugomas bendras akmenų skaičius). Taip pat 
+// atitinkamai modifikuokite metodus prideti1Akmeni(),  
+// pridetiDaugAkmenu(kiekis).
+
+class Kibiras1 {
+
+    static visiAkmenys = 0; // 7
+
+    static AKVK() {
+        console.log('AKVK', this.visiAkmenys); // 7
+    }
+    
+    constructor() {
+        this.akmenuKiekis = 0;
+    }
+
+    prideti1Akmeni() {
+        this.akmenuKiekis++;
+        this.constructor.visiAkmenys++; // 7
+    }
+
+    pridetiDaugAkmenu(kiekis) {
+        this.akmenuKiekis += kiekis;
+        this.constructor.visiAkmenys += kiekis; //7
+    }
+
+    kiekPririnktaAkmenu() {
+        console.log(this.akmenuKiekis)
+    }
+
+    akmenuSkaiciusVisuoseKibiruose(){
+        console.log(this.constructor.visiAkmenys) //7
+    }
+
+}
+
+// const k1 = new Kibiras1();
+// const k2 = new Kibiras1();
+// const k3 = new Kibiras1();
+
+// k1.prideti1Akmeni();
+// k3.pridetiDaugAkmenu(8);
+
+// k3.prideti1Akmeni();
+// k3.prideti1Akmeni();
+
+// k1.kiekPririnktaAkmenu();
+// k2.kiekPririnktaAkmenu();
+// k3.kiekPririnktaAkmenu();
+
+// k2.akmenuSkaiciusVisuoseKibiruose(); //7
+
+// Kibiras1.AKVK() //7
+
+// -----------------////-----------------
+
+// 4. Sukurti klasę Div. Sukūrus naują objektą iš šios klasės 
+// HTML’e turi atsirasti naujas <div> tagas su rand 1000 - 9999 
+// skaičiumi viduje. Sukurti metodą spalva(color), kuris keistų to 
+// <div> tago spalvą. Taip pat statinį metodą visuSpalva(color), 
+// kuris keistų visų <div> tagų, sukurtų per klasę, spalvą.
+
+class Div {
+
+    constructor() {
+        const bin = document.querySelector('#bin');
+        this.div = document.createElement('div');
+        const text = document.createTextNode(this.rand(1000, 9999));
+        this.div.append(text);
+        bin.append(this.div);
+        this.div.addEventListener('click', _ => this.changeColor(this.randColor()))
+
+    }
+
+    rand(min = 1000, max = 9999) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    changeDigit(digit){
+        this.div.innerText = digit;
+    }
+
+    randColor() {
+        return '#' + Math.floor(Math.random()*16777215).toString(16).padEnd(6, '0');
+    }
+
+    changeColor(color){
+        this.div.style.color = color;
+        this.div.style.borderColor = color;
+    }
+}
+
+const divs = [...Array(200)].map(_ => new Div());
+
+setInterval(_=> divs.forEach(d => d.changeColor(d.randColor()) || d.changeDigit(d.rand())), 500)
+
---
+++ 2023-04-03 hw/homework.js
@@ -0,0 +1,238 @@
+// 2. Sukurti klasę Pinigine. Konstruktoriuje sukurti dvi savybes 
+// popieriniaiPinigai ir metaliniaiPinigai. Parašyti metodą ideti
+// (kiekis), kuris prideda pinigus į piniginę. Jeigu kiekis 
+// nedidesnis už 2, tai prideda prie metaliniaiPinigai, 
+// jeigu kitaip- prie popieriniaiPinigai. Parašykite metodą 
+// skaiciuoti(), kuris suskaičiuotų ir išvestų į konsolę 
+// popieriniaiPinigai ir metaliniaiPinigai sumą. Sukurti klasės 
+// objektą ir pademonstruoti veikimą. Nesvarbu kokios popierinės 
+//kupiūros ir metalinės monetos egzistuoja realiame pasaulyje.
+
+// 6. Patobulinti 2 uždavinio piniginę taip, kad būtų galima skaičiuoti kiek piniginėje 
+// yra monetų ir kiek banknotų. Parašyti metodą monetos(), kuris skaičiuotų kiek yra 
+// piniginėje monetų ir metoda banknotai() - popierinių pinigų skaičiavimui. Kiekvieną 
+// atskirą dėjimą (ideti(kiekis) metodo kvietimą) laikykite vienu banknotu ar viena moneta.
+
+class Pinigine {
+    
+    constructor() {
+        this.popieriniaiPinigai = 0;
+        this.metaliniaiPinigai = 0;
+        this.visiPinigai = 0;
+        this.monetos1 = 0;
+        this.banknotai1 = 0;
+    }
+
+    ideti(kiekis) {
+        if(kiekis <= 2){
+            this.metaliniaiPinigai += kiekis
+            this.visiPinigai+= kiekis
+            this.monetos1++
+        } else {
+            this.popieriniaiPinigai += kiekis
+            this.visiPinigai+= kiekis
+            this.banknotai1++
+        }
+    }
+
+    skaiciuoti() {
+        console.log("Pinigu suma pinigineje:", this.visiPinigai);
+    };
+
+    monetos() {
+        console.log("monetu skaicius pinigineje:", this.monetos1)
+    };
+
+    banknotai() {
+        console.log("banknotu skaicius pinigineje:", this.banknotai1)
+    };
+}
+
+const p1 = new Pinigine();
+
+p1.ideti(3);
+p1.ideti(1);
+p1.ideti(4);
+
+p1.skaiciuoti();
+p1.monetos();
+p1.banknotai();
+
+
+// 3. Sukurti klasę Troleibusas. Konstruktoriuje sukurti savybę keleiviuSkaicius, 
+// kuri yra lygi 0. Parašyti du metodus: ilipa(keleiviuSkaicius) ir islipa(keleiviuSkaicius). 
+// O taip pat parašyti metoda vaziuoja(), kuris į konsolę išvestų troleibusu važiuojančių 
+// keleivių skaičių. Atkreipkite dėmesį, kad troleibusu važiuoti neigiamas keleivių 
+// skaičius negali.
+
+class Troleibusas {
+
+    constructor() {
+        this.keleiviuSkaicius = 0;
+        this.vaziuojanciuSk = 0;
+    }
+
+    ilipa(skaicius) {
+        this.keleiviuSkaicius += skaicius;
+
+
+        if (this.keleiviuSkaicius < 0){
+            this.keleiviuSkaicius = 0;
+        }
+    }
+
+    islipa(skaicius) {
+        this.keleiviuSkaicius -= skaicius;
+
+        if (this.keleiviuSkaicius < 0){
+            this.keleiviuSkaicius = 0;
+        }     
+        
+    }
+
+    vaziuoja() {
+        console.log("autobusu vaziuojanciu zmoniu skaicius:", this.keleiviuSkaicius)
+    }
+}
+
+const k1 = new Troleibusas()
+
+k1.islipa(3)
+k1.ilipa(8)
+
+k1.vaziuoja()
+
+// 8. Sukurti klasę Stikline. Sukurti savybes turis ir kiekis. Turis turi būti pasirenkamas 
+// objekto kūrimo metu. Parašyti metodą ipilti(kiekis), kuris keistų savybę kiekis. Jeigu 
+// stiklinės tūris yra mažesnis nei pilamas kiekis- kiekis netelpa ir būna lygus tūriui. 
+// Parašyti metodą ispilti(), kuris grąžiną kiekį. Pilant išpilamas visas kiekis, tas kas 
+// netelpa, nuteka per stalo viršų.  Sukurti metodą stiklinejeYra(), kuris į konsolę 
+// atspausdintų kiek stiklinėje yra skysčio. Sukurti tris stiklinės objektus su tūriais: 
+// 200, 150, 100. Didžiausią pripilti pilną ir tada ją ispilti į mažesnę stiklinę, o 
+// mažesnę į dar mažesnę.
+
+class Stikline {
+
+    constructor(turis) {
+        this.turis = turis;
+        this.kiekis = 0;
+    }
+
+    ipilti(kiekis) {
+        this.kiekis += kiekis;
+
+        if(this.turis < kiekis)
+            this.kiekis = this.turis;
+        
+        console.log("I stikline ipilame:", kiekis)
+    }
+
+    ispilti() {
+        let likutis = this.turis - this.kiekis;
+        let ispilamasKiekis = this.turis - likutis;
+        console.log('ispilame:', ispilamasKiekis)
+    }
+
+    stiklinejeYra() {
+        console.log('stiklineje yra kiekis:', this.kiekis)
+    }
+}
+
+const stikline1 = new Stikline(200);
+const stikline2 = new Stikline(150);
+const stikline3 = new Stikline(100);
+
+stikline1.ipilti(200);
+stikline1.stiklinejeYra();
+stikline1.ispilti();
+
+stikline2.ipilti(200)
+stikline2.stiklinejeYra();
+stikline2.ispilti()
+
+stikline3.ipilti(150);
+stikline3.stiklinejeYra();
+stikline3.ispilti()
+
+
+// 9. Sukurti klasę Grybas. Sukurti klasę Krepsys. Krepsys, kuri 
+// turi savybę dydis,kuriai konstruktoriuje yra priskiriama reikšmė 
+// 500 ir savybę prikrauta (kuri pradžioje lygi 0). Grybas turi tris 
+// savybes, kurios taip pat yra paskaičiuojamos konstruktoriuje: 
+// valgomas, sukirmijes, svoris. Kuriant Grybo objektą jo savybės 
+// turi būti atsitiktinai (rand funkcija) priskiriamos taip: valgomas- 
+// true arba false, sukirmijes- true arba false ir svoris- nuo 5 iki 45. 
+// Eiti grybauti, t.y. Kurti naujus Grybas objektus, jeigu nesukirmijęs 
+// ir valgomas dėti į Krepsi objektą, t.y. Vykdyti deti(grybas) metodą 
+// kol bus pririnktas pilnas krepšys nesukirmijusių ir valgomų grybų 
+// (gali būti truputį daugiau nei dydis).
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+class Grybas {
+
+    constructor() {
+        this.valgomas = Boolean(Math.round(Math.random()));
+        this.sukirmijes = Boolean(Math.round(Math.random()));
+        this.svoris = rand(5,45);
+    }
+
+}
+
+class Krepsys {
+    constructor() {
+      this.grybai = [];
+      this.dydis = 500;
+      this.prikrauta = 0;
+    }
+  
+    deti(grybas) {
+      if (grybas.valgomas && !grybas.sukirmijes) {
+        if (this.dydis >= grybas.svoris) {
+          this.grybai.push(grybas);
+          this.dydis -= grybas.svoris;
+          this.prikrauta++;
+        } else {
+          console.log("Krepšys pilnas");
+        }
+      } else {
+        console.log("Grybas netinka");
+      }
+    }
+  }
+
+
+  const krep = new Krepsys();
+
+  const gryb1 = new Grybas();
+  const gryb2 = new Grybas();
+  const gryb3 = new Grybas();
+  const gryb4 = new Grybas();
+  const gryb5 = new Grybas();
+  const gryb6 = new Grybas();
+  const gryb7 = new Grybas();
+  const gryb8 = new Grybas();
+  const gryb9 = new Grybas();
+  const gryb10 = new Grybas();
+  
+  console.log(gryb1);
+  console.log(gryb2);
+  console.log(gryb3);
+  console.log(gryb4);
+  
+  krep.deti(gryb1);
+  krep.deti(gryb2);
+  krep.deti(gryb3);
+  krep.deti(gryb4);
+  krep.deti(gryb5);
+  krep.deti(gryb6);
+  krep.deti(gryb7);
+  krep.deti(gryb8);
+  krep.deti(gryb9);
+  krep.deti(gryb10);
+  
+  console.log(krep.grybai);
